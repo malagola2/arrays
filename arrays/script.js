6e1523186580b0509c4b2ac9189873187238a32f
RAFAEL SARAGOCA MALAGOLA <rafael.smalagola@senacsp.edu.br>
2024-10-07 17:20:46 -0300

    first commit

---
+++ arrays/script.js
@@ -0,0 +1,10 @@
+let fruta =  ["abacate", "Kiwi", "morango", "uva" ];
+
+console.log(fruta[0]);
+fruta.push("manga");
+console.log(fruta[fruta.length -1]); // último elemento array
+console.log(fruta);
+
+for(let i = 0; i < fruta.length; i++) {
+    console.log(fruta[i]);
+}
